1b192ee5af50f3b5f71dd028ddf8f213d6e6c864
BrunoRagner <brunooragnner@gmail.com>
2021-09-09 13:01:20 -0300

    at01
    atualização

---
+++ tela/resources/views/layouts/main.blade.php
@@ -0,0 +1,31 @@
+
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="/css/style.css">
+<link rel="stylesheet" href="">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@500&display=swap" rel="stylesheet">
+<link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+<script src="/js/scripts.js"></script>
+<title>@yield('title')</title>
+</head>
+<body>
+    @yield('content')
+
+<footer>
+
+<p> eletry tecnologias  &copy; 2021</p>
+
+
+</footer>
+
+</body>
+</html>
+
+
+    </body>
+
+</html>
